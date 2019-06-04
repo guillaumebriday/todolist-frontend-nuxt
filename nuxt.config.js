29c616d244b5b4ad91912d3f92391f72ff67325b
Guillaume Briday <guillaumebriday@gmail.com>
2019-06-05 00:49:36 +0200

    disable eslint on save

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,15 +66,6 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
     }
   }
 }
