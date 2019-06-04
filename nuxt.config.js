455fb6797335f8849831c5b6842fe346dd9ee837
Guillaume Briday <guillaumebriday@gmail.com>
2019-06-05 00:49:24 +0200

    Adding font-awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    { src: '~/plugins/font-awesome.js', ssr: false }
+  ],
 
   /*
    ** Nuxt.js modules
---
+++ plugins/font-awesome.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+  faTimes,
+  faSpinner,
+  faCheck,
+  faUser,
+  faKey,
+  faTrash,
+  faPlus,
+  faLock,
+  faHeart,
+  faMobileAlt,
+  faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faClock, faLightbulb, faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faTimes, faSpinner, faCheck, faUser, faKey, faTrash, faPlus, faLock, faHeart, faMobileAlt, faExclamationTriangle)
+library.add(faGithub)
+library.add(faClock, faLightbulb, faCheckCircle)
+
+Vue.component('Fa', FontAwesomeIcon)
