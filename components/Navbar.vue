e92f472a873d36bbe06a0e261cc8204c7d936a38
Guillaume Briday <guillaumebriday@gmail.com>
2019-07-13 13:24:23 +0200

    Adding a Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav v-if="isLogged" class="bg-indigo">
+    <div class="container mx-auto px-4 py-4">
+      <div class="flex justify-between">
+        <div>
+          <nuxt-link class="text-white no-underline font-bold text-3xl hover:underline" to="/" exact>
+            Todolist
+          </nuxt-link>
+        </div>
+
+        <div>
+          <button class="text-grey-light ml-4 border border-white py-2 px-4 rounded hover:bg-white hover:text-indigo" @click="logout">
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: mapGetters({
+    isLogged: 'auth/isLogged'
+  }),
+
+  methods: {
+    logout () {
+      this.$axios.$delete('/auth/logout')
+      this.$store.dispatch('auth/logout')
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="bg-grey-lighter font-sans">
+    <navbar />
+
     <nuxt class="animated" />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
+  components: {
+    Navbar
+  },
+
   head () {
     return {
       title: this.$route.name,
